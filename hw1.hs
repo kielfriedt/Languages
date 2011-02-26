b2dfe5b0b1d3b458d05dc01febaa163cb5c00923
Kiel Friedt <kiel.friedt@gmail.com>
2011-02-26 00:54:29 -0800

    added comments

--- hw1.hs
+++ hw1.hs
@@ -14,10 +14,27 @@
  --    You should have received a copy of the GNU General Public License
  --    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --
--- cs 381, Summer 2010, homework 2
+-- cs 381, Summer 2010, homework 2-1
 -- Kiel Friedt
+--
+--Mini Logo is an extremely simplified version of the Logo language for programming 2D graphics. The idea
+--behind Logo and Mini Logo is to describe simple line graphics through commands to move a pen from one
+--position to another. The pen can either be “up” or “down”. Positions are given by pairs of integers. Macros
+--can be defined to reuse groups of commands. The syntax of Mini Logo is as follows.
+--
+--prog ::= cmd ; prog |
+--cmd ::= pen mode
+--    | moveto (pos,pos)
+--    | def name ( pars ) prog
+--    | call name ( vals )
+--mode ::= up | down
+--pos ::= num | name
+--pars ::= name, pars |
+--vals ::= num, vals |
 
 module hw1 where
+
+-- abstract syntax for Mini Logo as a Haskell data type
 
 -- 1.a)
 prog = [Cmd]
@@ -31,8 +48,14 @@ data Pos = I Int | S String
 type Pars = [String]
 type Vals = [Int]
 
+--Mini Logo macro vector that draws a line from a given position (x1,y1) to a given position
+--(x2,y2) and represent the macro in abstract syntax
+
 -- 1.b)
 vector = Def "vector" [x1, y1, x2, y2] [ pen down, moveto(x1,y1), moveto(x2,y2), pen up]
+
+--Haskell function steps :: Int -> Prog that constructs a Mini Logo program which draws a
+--stair of n steps.
 
 --1.c)
 
@@ -44,6 +67,17 @@ stairs :: Int -> prog
 stairs 0 = []
 stairs n = [ moveto(I n-1, I n),moveto(I n-1, I n-1)] ++ stairs(n-1)
 
+-- ______________________________________________________________________________________________________________________
+
+--The “Digital Circuits Design Language” whose syntax is shown below can be used to describe circuits built
+--from these gates.
+--circuit ::= gates; links
+--gates ::= num:gate ; gates | empty
+--gate ::= and | or | xor | not
+--links ::= from num.num to num.num; links | empty
+
+--abstract syntax for the above language as a Haskell data type.
+
 -- 2.a)
 
 type Circuit = ( Gates, Links)
@@ -52,16 +86,32 @@ data Gate = And | Or |Xor | Not
 type Links = [Link]
 data Link  = L (Int, Int) (Int, Int)
 
+--Represent a half adder circuit in abstract syntax
+
 -- 2.b)
 ([[1, Xor], [2, And]], [ L (1,1) (2,1) , L ( 1,2) (2,2)])
+
+--_____________________________________________________________________________________________________
+--data Expr = N Int
+--                | Plus Expr Expr
+--                | Times Expr Expr
+--                | Neg Expr
+--
+
+--Represent the expression -(3+4)*7 in the alternative abstract syntax
 
 -- 3.a)
 
 x =  Times(Neg((Plus (N 3) (N 4))) (N 7))
 
+--advantages or disadvantages of either representation
+
 -- 3.b) I found that the first abstract syntax was much easier to understand mostly
 --    because thats what i have seen in class. But i think that once i understand
 --    the second one it would be easier to write out.
+
+--Define a function translate :: Expr -> Exp that translates expressions given in the first abstract syntax
+--into equivalent expressions in the second abstract syntax
 
 -- 3.c)
 
